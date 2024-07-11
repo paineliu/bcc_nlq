f97146a306a5759dc191a80ce2531befc117ea78
paineliu <liutingchao@hotmail.com>
2024-07-11 16:44:18 +0800

    make_data finished.

--- make_data.py
+++ make_data.py
@@ -1,11 +1,110 @@
 import re
 import pkuseg
 import random
+import json
+import unicodedata
+from random import sample
+
+
+def is_chinese(char):
+    if 'CJK' in unicodedata.name(char):
+        return True
+    else:
+        return False
+
+def is_chinese_word(word):
+    for char in word:
+        if not is_chinese(char):
+            return False
+    return True
+
+def add_limit_list(token, num, bcc_limit):
+    bcc_limit.append("{{len(${})={}}}".format(num, len(token)))
+    bcc_limit.append("{{len(${})!={}}}".format(num, len(token) + 1))
+    if len(token) > 1:
+        bcc_limit.append("{{len(${})>{}}}".format(num, len(token) - 1))
+        bcc_limit.append("{{len(${})<{}}}".format(num, len(token) + 1))
+        bcc_limit.append("{{begin(${})=[{}]}}".format(num, ' '.join(get_zi_rand_list(token[0]))))
+        bcc_limit.append("{{end(${})=[{}]}}".format(num, ' '.join(get_zi_rand_list(token[-1]))))
+        bcc_limit.append("{{begin(${})!=[{}]}}".format(num, ' '.join(get_zi_not_rand_list(token[0]))))
+        bcc_limit.append("{{end(${})!=[{}]}}".format(num, ' '.join(get_zi_not_rand_list(token[-1]))))
+    if len(token) > 2:
+        bcc_limit.append("{{middle(${})=[{}]}}".format(num, ' '.join(get_zi_rand_list(token[1]))))
+        bcc_limit.append("{{middle(${})!=[{}]}}".format(num, ' '.join(get_zi_not_rand_list(token[1]))))
+
+def cut_sample(bcc_list, a, b):
+    ret_list = []
+    for tokens in bcc_list:
+        if a == b:
+            ret_list.append(' '.join(tokens[max(a - 1, 0): min(a + 2, len(tokens))]))
+        else:
+            ret_list.append(' '.join(tokens[a: b + 1]))
+    return ret_list
 
 def get_ci_rand_list(ci):
+    ci_data = []
+    f = open('./data/phrase.txt', encoding='utf-8')
+    for each in f:
+        items = each.split()
+        if is_chinese_word(items[0]):
+            ci_data.append(items[0])
+            if len(ci_data) >= 1000:
+                break
+            
     ci_list = []
     ci_list.append(ci)
+    total = random.randint(2, 4)
+    for i in range(100):
+        t = random.choice(ci_data)
+        if t not in ci_list:
+            ci_list.append(t)
+        if len(ci_list) >= total:
+            break
+
     return ci_list
+
+def get_zi_rand_list(zi):
+    zi_data = []
+    f = open('./data/phrase.txt', encoding='utf-8')
+    for each in f:
+        items = each.split()
+        if len(items[0]) == 1 and is_chinese_word(items[0]):
+            zi_data.append(items[0])
+            if len(zi_data) >= 1000:
+                break
+            
+    zi_list = []
+    zi_list.append(zi)
+    total = random.randint(2, 4)
+    for i in range(100):
+        t = random.choice(zi_data)
+        if t not in zi_list:
+            zi_list.append(t)
+        if len(zi_list) >= total:
+            break
+
+    return zi_list
+
+def get_zi_not_rand_list(zi):
+    zi_data = []
+    f = open('./data/phrase.txt', encoding='utf-8')
+    for each in f:
+        items = each.split()
+        if len(items[0]) == 1 and is_chinese_word(items[0]):
+            zi_data.append(items[0])
+            if len(zi_data) >= 1000:
+                break
+            
+    zi_list = []
+    total = random.randint(2, 4)
+    for i in range(100):
+        t = random.choice(zi_data)
+        if t != zi and t not in zi_list:
+            zi_list.append(t)
+        if len(zi_list) >= total:
+            break
+
+    return zi_list
 
 def get_pos_rand_list(pos):
     pos_data = ['Ag','i','o','vn','a','j','p','w','ad','k','q','x','an','l','r','y','b','m','s','z','c','Ng','Tg','un','Dg','n','t','h','d','nr','u','g','e','ns','Vg','nz','f','nt','v','vd']
@@ -21,8 +120,19 @@ def get_pos_rand_list(pos):
             break
 
     return pos_list
+    
+# 生成简单的查询词
+def get_sample_phrase(tokens, a, b):
+    bcc_list = []
+    bcc_list.append(tokens[a][0])
+    if a != b:
+        bcc_list.append(tokens[b][0])
 
+    return bcc_list
+
+# 生成带词性限制的表达式
 def get_sample_pos_limit(tokens, a, b):
+    bcc_list = []
     bcc = []
     for i, item in enumerate(tokens):
         if (i == a):
@@ -31,27 +141,25 @@ def get_sample_pos_limit(tokens, a, b):
             bcc.append('{}/{}'.format(item[0], item[1]))
         else:
             bcc.append(item[0])
-    if a == b:
-        if a > 0:
-            return [' '.join(bcc[a-1: a+1])]
-        else:
-            return [' '.join(bcc[a: a+2])]
-    else:
-        return [' '.join(bcc[a: b+1])]
-    return [' '.join(bcc)]
+    bcc_list.append(bcc)
+    return cut_sample(bcc_list, a, b)
 
-def get_sample(tokens, a, b):
+# 将词语替换为词性
+def get_sample_pos(tokens, a, b):
+    bcc_list = []
     bcc = []
     for i, item in enumerate(tokens):
         if (i == a):
-            bcc.append('({})'.format(item[1]))
+            bcc.append('{}'.format(item[1]))
         elif (i == b):
-            bcc.append('({})'.format(item[1]))
+            bcc.append('{}'.format(item[1]))
         else:
             bcc.append(item[0])
-    return [' '.join(bcc)]
+    bcc_list.append(bcc)
+    return cut_sample(bcc_list, a, b)
 
-def get_sample_len(tokens, a, b):
+# 将词语替换为词性，再加入限定条件
+def get_sample_constraint(tokens, a, b):
     bcc_list = []
     bcc = []
     for i, item in enumerate(tokens):
@@ -61,48 +169,57 @@ def get_sample_len(tokens, a, b):
             bcc.append('({})'.format(item[1]))
         else:
             bcc.append(item[0])
-    if a == b:
-        bcc_len = bcc
-        if (tokens[a][1] != 'w'):
-            bcc_len.append("{{len($1)={}}}".format(len(tokens[a][0])))
-        bcc_list.append(' '.join(bcc_len))
-    else:
-        bcc_len = bcc
-        if (tokens[a][1] == 'w'):
-            bcc_len.append("{{len($2)={}}}".format(len(tokens[b][0])))
-        elif (tokens[b][1] == 'w'):
-            bcc_len.append("{{len($1)={}}}".format(len(tokens[a][0])))
-        else:
-            bcc_len.append("{{len($1)={};len($2)={}}}".format(len(tokens[a][0]),len(tokens[b][0])))
-        bcc_list.append(' '.join(bcc_len))
-    return bcc_list
+    bcc_list.append(bcc)
 
-def get_sample_match_len(tokens, a, b):
-    bcc_list = []
-    bcc = []
-    for i, item in enumerate(tokens):
-        if (i == a):
-            bcc.append('({})'.format(item[1]))
-        elif (i == b):
-            bcc.append('({})'.format(item[1]))
-        else:
-            bcc.append(item[0])
+    bcc_limit = []
+    bcc_limit.append("{{count>{}}}".format(random.randint(2, 10)))
+    bcc_limit.append("{{count<{}}}".format(random.randint(100, 1000)))
+    
     if a == b:
-        bcc_len = bcc
         if (tokens[a][1] != 'w'):
-            bcc_len.append("{{len($1)={}}}".format(len(tokens[a][0])))
-        bcc_list.append(' '.join(bcc_len))
+            add_limit_list(tokens[a][0], 1, bcc_limit)
     else:
-        bcc_len = bcc
-        if (tokens[a][1] == 'w'):
-            bcc_len.append("{{len($2)={}}}".format(len(tokens[b][0])))
-        elif (tokens[b][1] == 'w'):
-            bcc_len.append("{{len($1)={}}}".format(len(tokens[a][0])))
-        else:
-            bcc_len.append("{{len($1)={};len($2)={}}}".format(len(tokens[a][0]),len(tokens[b][0])))
-        bcc_list.append(' '.join(bcc_len))
-    return bcc_list
+        bcc_limit.append("{$1=$2}")
+        bcc_limit.append("{$1!=$2}")
 
+        if (tokens[a][1] == 'w'):
+            add_limit_list(tokens[b][0], 2, bcc_limit)
+        elif (tokens[b][1] == 'w'):
+            add_limit_list(tokens[a][0], 1, bcc_limit)
+        else:
+            add_limit_list(tokens[a][0], 1, bcc_limit)
+            add_limit_list(tokens[b][0], 2, bcc_limit)
+
+            bcc_limit.append("{{len($1)={}; len($2)={}}}".format(len(tokens[a][0]),len(tokens[b][0])))
+            bcc_limit.append("{{len($1)!={}; len($2)={}}}".format(len(tokens[a][0]) + 1,len(tokens[b][0])))
+            bcc_limit.append("{{len($1)={}; len($2)!={}}}".format(len(tokens[a][0]), len(tokens[b][0]) + 1))
+            bcc_limit.append("{{len($1)!={}; len($2)!={}}}".format(len(tokens[a][0]) + 1,len(tokens[b][0])+ 1))
+            
+            if len(tokens[a][0]) > 1 and len(tokens[b][0]) > 1:
+                bcc_limit.append("{{len($1)>{}; len($2)={}}}".format(len(tokens[a][0]) - 1, len(tokens[b][0])))
+                bcc_limit.append("{{len($1)>{}; len($2)>{}}}".format(len(tokens[a][0]) - 1, len(tokens[b][0]) - 1))
+                bcc_limit.append("{{len($1)<{}; len($2)>{}}}".format(len(tokens[a][0]) + 1, len(tokens[b][0]) - 1))
+                
+                bcc_limit.append("{{begin($1)=[{}]; len($2)={}}}".format(' '.join(get_zi_rand_list(tokens[a][0])), len(tokens[b][0])))
+                bcc_limit.append("{{end($1)=[{}]; len($2)={}}}".format(' '.join(get_zi_rand_list(tokens[a][-1])), len(tokens[b][0])))
+                bcc_limit.append("{{begin($1)!=[{}]; len($2)>{}}}".format(' '.join(get_zi_not_rand_list(tokens[a][0])), len(tokens[b][0]) - 1))
+                bcc_limit.append("{{end($1)!=[{}]; len($2)>{}}}".format(' '.join(get_zi_not_rand_list(tokens[a][-1])), len(tokens[b][0]) - 1))
+                
+                bcc_limit.append("{{len($1)={}; begin($2)=[{}]}}".format(len(tokens[a][0]), ' '.join(get_zi_rand_list(tokens[b][0]))))
+                bcc_limit.append("{{len($1)={}; end($2)=[{}]}}".format(len(tokens[a][0]), ' '.join(get_zi_rand_list(tokens[b][-1]))))
+                bcc_limit.append("{{len($1)>{}; begin($2)!=[{}]}}".format(len(tokens[a][0]) - 1, ' '.join(get_zi_not_rand_list(tokens[b][0]))))
+                bcc_limit.append("{{len($1)>{}; end($2)!=[{}]}}".format(len(tokens[a][0]) - 1, ' '.join(get_zi_not_rand_list(tokens[b][-1]))))
+
+    bcc_list = cut_sample(bcc_list, a, b)
+    bcc_result = []
+    for bcc in bcc_list:
+        for limit in bcc_limit:
+            bcc_data = '{}{}'.format(bcc, limit)
+            bcc_result.append(bcc_data)
+
+    return bcc_result
+
+# 处理通配符
 def get_sample_wildcard(tokens, a, b):
     bcc_list = []
     bcc = []
@@ -111,7 +228,7 @@ def get_sample_wildcard(tokens, a, b):
             bcc.append('.' * len(item[0]))
         else:
             bcc.append(item[0])
-    bcc_list.append(' '.join(bcc))
+    bcc_list.append(cut_sample([bcc], a, a)[0])
 
     bcc = []
     for i, item in enumerate(tokens):
@@ -119,72 +236,110 @@ def get_sample_wildcard(tokens, a, b):
             bcc.append('.' * len(item[0]) + '/{}'.format(item[1]))
         else:
             bcc.append(item[0])
-    bcc_list.append(' '.join(bcc))
-    
+    bcc_list.append(cut_sample([bcc], a, a)[0])
+
     bcc = []
     for i, item in enumerate(tokens):
         if (i == a):
             bcc.append('.' * len(item[0]) + '/[{} v]'.format(item[1]))
         else:
             bcc.append(item[0])
-    bcc_list.append(' '.join(bcc))
+    bcc_list.append(cut_sample([bcc], a, a)[0])
 
-    if (a != b):
+    bcc = []
+    for i, item in enumerate(tokens):
+        if (i == a):
+            bcc.append('~')
+        else:
+            bcc.append(item[0])
+    bcc_list.append(cut_sample([bcc], a, a)[0])
+
+    bcc = []
+    for i, item in enumerate(tokens):
+        if (i == a):
+            bcc.append('~' + '/[{} v]'.format(item[1]))
+        else:
+            bcc.append(item[0])
+    bcc_list.append(cut_sample([bcc], a, a)[0])
+
+    bcc = []
+    for i, item in enumerate(tokens):
+        if (i == a):
+            bcc.append('@')
+        else:
+            bcc.append(item[0])
+    bcc_list.append(cut_sample([bcc], a, a)[0])
+
+    bcc = []
+    for i, item in enumerate(tokens):
+        if (i == a):
+            bcc.append('@' + '/[{} v]'.format(item[1]))
+        else:
+            bcc.append(item[0])
+    bcc_list.append(cut_sample([bcc], a, a)[0])
+
+    if (a + 1 < b):
         bcc = []
         for i, item in enumerate(tokens):
-            if (i == b):
+            if (i == a):
+                bcc.append('.' * len(item[0]) + '/{}'.format(item[1]))
+            elif (i == b):
                 bcc.append('.' * len(item[0]) + '/{}'.format(item[1]))
             else:
                 bcc.append(item[0])
-        bcc_list.append(' '.join(bcc))
+        bcc_list.append(cut_sample([bcc], a, b)[0])
+        bcc = []
+        for i, item in enumerate(tokens):
+            if (i == a):
+                bcc.append('.' * len(item[0]))
+            elif (i == b):
+                bcc.append('.' * len(item[0]))
+            else:
+                bcc.append(item[0])
+        bcc_list.append(cut_sample([bcc], a, b)[0])
+
     return bcc_list
 
-def get_sample_interval(token, a, b):
-    pass
+def get_sample(tokens, a, b):
+    sample_list = []
+    sample_list += get_sample_phrase(tokens, a, b)
+    sample_list += get_sample_pos(tokens, a, b)
+    sample_list += get_sample_constraint(tokens, a, b)
+    sample_list += get_sample_pos_limit(tokens, a, b)
+    sample_list += get_sample_wildcard(tokens, a, b)
+    return sample_list    
 
-def get_sample_collect(token, a, b):
-    pass
-
-pku_seg = pkuseg.pkuseg(postag=True)           # 以默认配置加载模型
-        
-f = open('./data/rmrb/2014-01.txt', encoding='utf_8')
-for each in f:
-    each = each.strip()
-    sentences = re.split(r"([.。!！?？\s+])", each)
-    sentences.append("")
-    sentences = ["".join(i) for i in zip(sentences[0::2],sentences[1::2])]
-    for sen in sentences:
-        if len(sen) >=5 and len(sen) <= 15:
-            sen = '今天天气很不错！'
-            tokens = pku_seg.cut(sen)    # 进行分词和词性标注
-            a = random.randint(0, len(tokens) - 1)
-            b = a
-            if (random.random() < 0.3):
-                b = random.randint(0, len(tokens) - 1)
-                if a != b:
-                    print(a, b)
-                else:
-                    print(a)
-            else:
-                print(a)
-            if a > b:
-                c = b
+def make_sample_data(data_filename, out_filename):
+    pku_seg = pkuseg.pkuseg(postag=True)
+    sample_map = {}
+    f = open(data_filename, encoding='utf_8')
+    for each in f:
+        each = each.strip()
+        sentences = re.split(r"([.。!！?？\s+])", each)
+        sentences.append("")
+        sentences = ["".join(i) for i in zip(sentences[0::2],sentences[1::2])]
+        for sen in sentences:
+            if len(sen) >=5 and len(sen) <= 15:
+                tokens = pku_seg.cut(sen)    # 进行分词和词性标注
+                a = random.randint(0, len(tokens) - 1)
                 b = a
-                a = c
-            # 词性替换：一个加入词性限定；两个词语加入词性限定（可以是集合），另一个词替换为词性（可以是集合）
-            # 词性替换：一个、两个词语为词性，无约束
-            # 词性替换：加入约束条件（内容、频次、长度）。
-            # 通配符替换：，使用 . .. ~ @
-            # 通配符替换：，加入词性限定（可以是集合）
-            # 属性约束替换：（内容、词长、频率）
-            # 离合符替换：仅保留ab中间替换为*，a前替换为*， b后替换为*
-            # 集合数据替换：词列表、词性列表
 
-            # bcc = get_sample(tokens,  a, b)
-            print(get_pos_rand_list('a'))
-            print(get_pos_rand_list('a'))
-            print(get_pos_rand_list('v'))
-            print(get_pos_rand_list('v'))
-            
-            bcc = get_sample_pos_limit(tokens, a, b)
-            print(sen, tokens, a, b, bcc)
+                if (random.random() < 0.3):
+                    b = random.randint(0, len(tokens) - 1)
+                if a > b:
+                    c = b
+                    b = a
+                    a = c         
+                sample_list = get_sample(tokens, a, b)
+                sample_map[sen] = sample(sample_list, 10)
+                print(len(sample_map))
+
+        if len(sample_map)>=10000:
+            break
+    f = open(out_filename, 'w', encoding='utf_8')
+    json.dump(sample_map, f, ensure_ascii=False, indent=4)
+    f.close()
+
+if __name__ == '__main__':
+
+    make_sample_data('./data/rmrb/2014-01.txt', 'out.json')
