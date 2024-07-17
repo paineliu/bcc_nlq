250e95d3410c8b8bdd13eda60978b4a4dbd9d80b
paineliu <liutingchao@hotmail.com>
2024-07-17 09:02:59 +0800

    glm4

---
+++ glm4/train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+# 设置环境变量
+export CUDA_VISIBLE_DEVICES=2
+export NCCL_P2P_DISABLE="1"
+export NCCL_IB_DISABLE="1"
+ 
+# 执行 Python 脚本
+python finetune_hf.py  ./dataset_glm_tongyi  ./model/ZhipuAI/chatglm3-6b  ./configs/lora.yaml
