39fd839dbaa83feda32684219e66957c94b3981a
paineliu <liutingchao@hotmail.com>
2024-08-09 09:07:49 +0800

    add time

--- qwen/test_f.py
+++ qwen/test_f.py
@@ -1,5 +1,7 @@
 import os 
 import json
+import time
+
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  #（保证程序cuda序号与实际cuda序号对应）
 os.environ['CUDA_VISIBLE_DEVICES'] = "2"  #（代表仅使用第0，1号GPU）
 
@@ -17,6 +19,8 @@ def test_acc(test_filename, result_filename):
     f = open(test_filename, encoding='utf_8')
     j_data = json.load(f)
     o_data = []
+    time_start = time.time()  # 记录开始时间
+
     for each in j_data:
         response, history = model.chat(tokenizer, each['conversations'][0]['value'], history=None)
         data ={
@@ -34,6 +38,10 @@ def test_acc(test_filename, result_filename):
         }
         o_data.append(data)
         print(response)
+    time_end = time.time()  # 记录结束时间
+    time_sum = time_end - time_start  # 计算的时间差为程序的执行时间，单位为秒/s
+    print(time_sum)
+    print(time_sum / len(j_data))
     f_o = open(result_filename, 'w', encoding='utf_8')
     json.dump(o_data, f_o, ensure_ascii=False, indent=4)
     f_o.close()
